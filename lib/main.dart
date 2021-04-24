7481265076dd22ea9177aa84bd1ca4bb57998923
rodrigoms1718 <rodrigoms1718@gmail.com>
2021-04-24 14:58:18 -0300

    trabalhando com lista e botoes

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,33 @@ main() {
 @override
 class PerguntaApp extends StatelessWidget {
   Widget build(BuildContext context) {
+    final List<String> perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?',
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Perguntas'),
         ),
-        body: Text('Olá flutter'),
+        body: Column(
+          children: <Widget>[
+            Text(perguntas[0]),
+            ElevatedButton(
+              child: Text('Resposta 1'),
+              onPressed: null,
+            ),
+            ElevatedButton(
+              child: Text('Resposta 2'),
+              onPressed: null,
+            ),
+            ElevatedButton(
+              child: Text('Resposta 3'),
+              onPressed: null,
+            ),
+          ],
+        ),
       ),
     );
   }
